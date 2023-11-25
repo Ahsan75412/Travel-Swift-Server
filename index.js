e72fd7479646cdbf5fa2a14f591ae28001478c4c
Ahsan <ahsanpub16b@gmail.com>
2023-11-26 01:30:58 +0600

    Added here payments intregration with sripe and create review get and post API intregration

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 const jwt = require('jsonwebtoken');
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -13,7 +13,6 @@ app.use(express.json());
 
 //verify jWT
 
- 
 function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
@@ -21,11 +20,14 @@ function verifyJWT(req, res, next) {
     }
     const token = authHeader.split(" ")[1];
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    
         if (err) {
             return res.status(403).send("Forbidden");
         }
         req.decoded = decoded;
+
         next();
+        console.log('hello',decoded)
     });
 }
 
@@ -33,11 +35,9 @@ function verifyJWT(req, res, next) {
 
 // mongodb
 
-
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tb0uxuv.mongodb.net/?retryWrites=true&w=majority`;
+
+
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -53,6 +53,7 @@ const client = new MongoClient(uri, {
 
 
 
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -61,7 +62,9 @@ async function run() {
     const userCollection = client.db("TravelTourDB").collection("users");
     const hotelsCollection = client.db("TravelTourDB").collection("hotels");
     const ordersCollection = client.db("TravelTourDB").collection("orders");
+    const paymentsCollection = client.db("TravelTourDB").collection("payments");
     const infoCollection = client.db("TravelTourDB").collection("info");
+    const reviewCollection = client.db("TravelTourDB").collection("review");
 
 
 
@@ -80,7 +83,7 @@ async function run() {
   //make an admin api
   app.put(
       "/user/admin/:email",
-      // verifyJWT,
+      verifyJWT,
       // verifyAdmin,
       async (req, res) => {
           const email = req.params.email;
@@ -97,10 +100,25 @@ async function run() {
   );
 
 
+// payment routes
+        app.post("/create-payment-intent", verifyJWT, async (req, res) => {
+            const products = req.body;
+            const price = products.price;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                payment_method_types: ["card"],
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
+
   //make an host
   app.put(
       "/user/host/:email",
-      // verifyJWT,
+      verifyJWT,
       // verifyAdmin,
       async (req, res) => {
           const email = req.params.email;
@@ -154,6 +172,9 @@ app.put("/user/:email", async (req, res) => {
     );
     res.send({ result, token });
 });
+
+
+
 
 app.get("/user", async (req, res) => {
     const users = await userCollection.find({}).toArray();
@@ -235,33 +256,31 @@ app.get("/user", async (req, res) => {
 
 
 
-        // post new order
-      //   app.post("/orders", async (req, res) => {
-      //     const order = req.body;
-      //     const result = await ordersCollection.insertOne(order);
-      //     res.send(result);
-      // });
+   
 
-
-
-
-
-
-    // manage hotels
 
      //get all orders
-     app.get("/orders", verifyJWT, async (req, res) => {
-      const email = req.query.email;
-      const decodedEmail = req.decoded.email;
-      if (email === decodedEmail) {
-          const query = { email: email };
-          const cursor = ordersCollection.find(query);
-          const orders = await cursor.toArray();
-          return res.send(orders);
-      } else {
-          return res.status(403).send("Forbidden");
-      }
-  });
+     app.get("/orders",verifyJWT, async (req, res) => { //
+        const email = req.query.email;
+        console.log(email)
+        const decodedEmail = req.decoded.email;
+        console.log(decodedEmail)
+        if (email === decodedEmail) {
+            const query = { email: email };
+            const cursor = ordersCollection.find(query);
+            const orders = await cursor.toArray();
+            return res.send(orders);
+        } else {
+            return res.status(403).send("Forbidden");
+        }
+    });
+ 
+
+    
+
+
+
+
   app.get("/allOrders",  async (req, res) => {  //verifyJWT,
       const query = {};
       const cursor = ordersCollection.find(query);
@@ -269,12 +288,21 @@ app.get("/user", async (req, res) => {
       res.send(orders);
   });
 
+
+
+
+
   app.get("/orders/:id", async (req, res) => { // verifyJWT,
       const id = req.params.id;
-      const query = { _id: ObjectId(id) };
+      const query = { _id:new ObjectId(id) };
       const orders = await ordersCollection.findOne(query);
       res.send(orders);
   });
+
+
+
+
+
 
   // post new order
   app.post("/orders", async (req, res) => {
@@ -283,11 +311,18 @@ app.get("/user", async (req, res) => {
       res.send(result);
   });
 
+
+
+
+
+
+  //payment isSuccessfull!
+
   app.patch("/orders/:id", verifyJWT, async (req, res) => {
       const id = req.params.id;
       const payment = req.body;
 
-      const query = { _id: ObjectId(id) };
+      const query = { _id: new ObjectId(id) };
       const updatedDoc = {
           $set: {
               paid: true,
@@ -301,6 +336,10 @@ app.get("/user", async (req, res) => {
       );
       res.send(updatedDoc);
   });
+
+
+
+
 
   //approve order
   app.put("/orders/status/:id", async (req, res) => {
@@ -318,7 +357,18 @@ app.get("/user", async (req, res) => {
 
 
 
-
+  //add a review
+  app.post("/reviews", async (req, res) => {
+    const review = req.body;
+    const result = await reviewCollection.insertOne(review);
+    res.send(result);
+});
+// get all review
+app.get("/reviews", async (req, res) => {
+    const cursor = reviewCollection.find({});
+    const reviews = await cursor.toArray();
+    res.send(reviews);
+});      
 
   // delete a product
 
@@ -370,20 +420,9 @@ app.get("/user", async (req, res) => {
       res.send(result);
   });
 
-  //add a review
-  // app.post("/reviews", async (req, res) => {
-  //     const review = req.body;
-  //     const result = await reviewCollection.insertOne(review);
-  //     res.send(result);
-  // });
 
 
-  // get all review
-  // app.get("/reviews", async (req, res) => {
-  //     const cursor = reviewCollection.find({});
-  //     const reviews = await cursor.toArray();
-  //     res.send(reviews);
-  // });
+
 
 
 
@@ -408,11 +447,8 @@ app.get("/user", async (req, res) => {
 
 
 
-
-
-
   // update profile
-  app.put("/info", async (req, res) => {
+  app.put("/info", async (req, res) => { 
       const email = req.query.email;
 
       const query = { email: email };
@@ -431,7 +467,6 @@ app.get("/user", async (req, res) => {
                   studyIn: newStudyIn,
                   phone: newPhone,
                   Tweeeter: newTweeeter,
-                //   github: newGithub,
                   facebook: newFacebook,
               },
           },
